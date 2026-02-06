ff4ce5c7b8139d282a1351320be67b31272675e6
Tomas Lin <tomaslin@gmail.com>
2026-02-06 04:01:32 -0800

    add names.js

---
+++ names.js
@@ -0,0 +1,19 @@
+ const nameToUrl = {
+            "Robin & Dagny": "https://www.facebook.com/robinthomastango",
+            "Roxana & Cesar": "https://youtu.be/6wZbMCAeGGk?si=glY3ONv8d-DBEXMh",
+            "Paloma & Maxi": "http://www.palomaymaximiliano.com",
+            "Cecilia Gonzalez": "https://tangomotion.com",
+            "Hugo & Celina": "https://hugoandcelina.com/",
+            "Florencia & Marcos": "http://www.marcosyflorencia.com/",
+            "Avik": "http://www.a2tango.com/teachers.html",
+            "Koichiro": "https://www.pghtango.com/about-pgh-tango",
+            "Brick": "https://www.facebook.com/brick.robbins",
+            "Victoria": "https://www.facebook.com/victoria.randlett",
+            "Alex": "https://www.tangoberretin.com/alexkrebs",
+            "Steve Correl": "https://www.facebook.com/steve.correl.1/",
+            "Stephen Brady": "https://www.facebook.com/steve.bradley.5015/",
+            "Andy Stein": "https://www.facebook.com/andris.stein/",
+            "Robin": "http://robinthomastango.com/w/robin/",
+            "Rob": "https://www.facebook.com/rob.montango",
+            "Michael D": "https://www.facebook.com/michael.daniels.337582"
+        };
